ee196a5e4d4e4072d1cccb24b1f1b3ef4f9503e3
Moi <Moi@DESKTOP-6VR2MS0>
2022-07-25 19:17:31 +0200

    fix flag

--- location/countries.php
+++ location/countries.php
@@ -35,7 +35,7 @@ $response = curl_exec($client);
 				$result = json_decode($response);
 				foreach ($result->items as $country) {
 				    echo "<tr><td>".$country->id."</td><td>".$country->name."<br/><font size='1' color='gray'>".$country->codeiso2." / ".$country->codeiso3." / ".$country->number."</font></td>
-<td>".$country->fullname." </td><td><A href='continentDetails.php?id=".$country->continent->id."'/>".$country->continent->name."</A></td><td><A href='regionDetails.php?id=".$country->region->id."'/>".$country->region->name."</A></td><td><img src='../flags/16x16/".$country->codeiso2.".png'/></td><td><A href='countryDetails.php?id=".$country->id."'>Detail</A></td></tr>";
+<td>".$country->fullname." </td><td><A href='continentDetails.php?id=".$country->continent->id."'/>".$country->continent->name."</A></td><td><A href='regionDetails.php?id=".$country->region->id."'/>".$country->region->name."</A></td><td><img src='../flags/16x16/".strtolower($country->codeiso2).".png'/></td><td><A href='countryDetails.php?id=".$country->id."'>Detail</A></td></tr>";
 				}?>
 				</tbody>
 				</table>
