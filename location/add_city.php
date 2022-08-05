dd4161e4030a5139b761ed4df77f8c87890162e1
Moi <Moi@DESKTOP-6VR2MS0>
2022-08-05 15:52:12 +0200

    Add creation city page

---
+++ location/add_city.php
@@ -0,0 +1,61 @@
+<!doctype html>
+<html>
+<head>
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<meta name="description"
+	content="A layout example that shows off a responsive bball layout.">
+<link rel="stylesheet"
+	href="https://unpkg.com/purecss@2.1.0/build/pure-min.css"
+	integrity="sha384-yHIFVG6ClnONEA5yB5DJXfW2/KC173DIQrYoZMEtBvGzmf0PKiGyNEqe9N6BNDBH"
+	crossorigin="anonymous">
+<link rel="stylesheet" href="styles.css">
+</head>
+<body>
+
+<?php
+if(isset($_POST["cityName"]) && isset($_POST["countryName"])){
+    $configs = include('../config.php');
+    $cityName = $_POST["cityName"];
+    $countryName = $_POST["countryName"];
+    $url = $configs["endpoint.location.city"];
+    $curl = curl_init($url);
+    curl_setopt($curl, CURLOPT_URL, $url);
+    curl_setopt($curl, CURLOPT_POST, true);
+    curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+    
+    $headers = array("Accept: application/json","Content-Type: application/json",);
+    curl_setopt($curl, CURLOPT_HTTPHEADER, $headers);
+    
+    $data = "{\"name\": \"".$cityName."\",\"countryname\": \"".$countryName."\"}";
+    curl_setopt($curl, CURLOPT_POSTFIELDS, $data);
+
+    $resp = curl_exec($curl);
+    curl_close($curl);
+}
+
+?>
+
+
+	<div id="layout" class="content pure-g">
+	<?php include('menu.php');?>
+    <div id="main" class="pure-u-1">
+			<div class="bball-content">
+				<div class="bball-content-header pure-g">
+					<div class="pure-u-1-2">
+						<h1>Add new city</h1>
+					</div>
+				</div>
+
+				<form action="add_city.php" method="post">
+					<label for="cityName">City Name:</label> <input type="text"
+						id="cityName" name="cityName"><br>
+					<br> <label for="countryName">Last name:</label> <input type="text"
+						id="countryName" name="countryName"><br>
+					<br> <input type="submit" value="Submit">
+				</form>
+			</div>
+		</div>
+	</div>
+
+</body>
+</html>
