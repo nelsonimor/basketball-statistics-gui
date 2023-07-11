c3ff682ad8ed53a551f25fe08f1a39fd2353dfd8
JeremLCA <Moi@DESKTOP-6VR2MS0>
2023-07-11 16:50:06 +0200

    Use flag returned by service

--- location/countries_table.php
+++ location/countries_table.php
@@ -5,13 +5,14 @@
 				<tbody>
 				<?php
 				$result = json_decode($response);
+				//echo $response;
 				foreach ($result->items as $country) {
 				    echo "<tr><td>".$country->id."</td>
                         <td>".$country->name."<br/><font size='1' color='gray'>".$country->codeiso2." / ".$country->codeiso3." / ".$country->number."</font></td>
                         <td>".$country->fullname." </td>
                         <td><A href='continentDetails.php?id=".$country->continent->id."'/>".$country->continent->name."</A></td>
                         <td><A href='regionDetails.php?id=".$country->region->id."'/>".$country->region->name."</A></td>
-                        <td><img src='../flags/16x16/".strtolower($country->codeiso2).".png'/></td>
+                        <td><img src='".$country->flagurl."'/> </td>
                         <td><A href='countryDetails.php?id=".$country->id."'><img src='../icon/detail.png' height='16px' width='16px'/></A></td></tr>";
 				}?>
 				</tbody>
