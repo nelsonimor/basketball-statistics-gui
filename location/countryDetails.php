021df4cd6c256af7fc28c6165fed3acef8d4285f
Jerem GitBash <jeremtruc31@hotmail.fr>
2024-09-10 20:28:06 +0200

    Add call to link to nat

--- location/countryDetails.php
+++ location/countryDetails.php
@@ -17,6 +17,7 @@ $client = curl_init($url);
 curl_setopt($client,CURLOPT_RETURNTRANSFER,true);
 $response = curl_exec($client);
 $result = json_decode($response);
+$countryName = $result->name;
 ?>
 
 <div id="layout" class="content pure-g">
@@ -25,12 +26,76 @@ $result = json_decode($response);
         <div class="bball-content">
             <div class="bball-content-header pure-g">
                 <div class="pure-u-1-2">
-                    <h1><?php echo "<h1>Country : ".$result->name." (".$result->fullname.")</h1>";?></h1>
+                    <h1><?php echo "<h1>Country : ".$countryName." (".$result->fullname.")</h1>";?></h1>
                 </div>
             </div>
         </div>
     </div>
 </div>
+
+<?php
+$configs = include('../config.php');
+$url = $configs["endpoint.location.persons"]."?birthcountryId=".$id;
+$client = curl_init($url);
+curl_setopt($client,CURLOPT_RETURNTRANSFER,true);
+$response = curl_exec($client);
+?>
+
+<div id="layout" class="content pure-g">
+	<?php include('menu.php');?>
+    <div id="main" class="pure-u-1">
+        <div class="bball-content">
+			<div class="bball-content-body">
+				<h2><?php echo "<h3>".count(json_decode($response)->items)." persons born in : ".$countryName."</h3>";?></h2>
+				<table class="pure-table">
+				<thead>
+				<tr><th>Id</th><th>Name</th><th>Height</th><th>Hand</th><th>Birth place</th><th>Birth date</th><th>Nationality</th><th>Detail</th></tr>
+				</thead>
+				<tbody>
+				<?php
+				$result = json_decode($response);
+				foreach ($result->items as $person) {
+				echo "<tr>
+                    <td>".$person->id."</td>
+                    <td>".$person->lastname." ".$person->firstname."</td>";
+                echo "<td>";
+                if(isset($person->height) && $person->height!=null){
+                    echo $person->height;
+                }
+                echo "</td>";
+                echo   "<td>".$person->hand."</td>";
+               
+				if(isset($person->birthplacecity->country)){
+				    echo "<td><img src='".$person->birthplacecity->country->flagurl."'/> ".$person->birthplacecity->name.", ".$person->birthplacecity->country->name."</td>";
+				}
+				else{
+				    echo "<td></td>";
+				}
+				
+
+                    
+
+
+
+                echo "<td>".$person->birthdate."</td>
+                    <td>
+                    <img src='".$person->firstnationality->flagurl."'/>";
+				if (isset($person->secondnationality->flagurl))echo " <img src='".$person->secondnationality->flagurl."'/>";
+				if (isset($person->thirdnationality->flagurl))echo " <img src='".$person->thirdnationality->flagurl."'/>";
+                echo    "<td><A href='personDetails.php?id=".$person->id."'>Detail</th></td>
+                    </tr>";
+				}?>
+				</tbody>
+				</table>
+			</div>
+        </div>
+    </div>
+</div>
+
+
+
+
+
 
 <?php 
 $url = $configs["endpoint.location.city"]."?countryId=".$id;
@@ -44,6 +109,7 @@ $response = curl_exec($client);
     <div id="main" class="pure-u-1">
         <div class="bball-content">
 			<div class="bball-content-body">
+			<h2><?php echo "<h3>".count(json_decode($response)->items)." cities declared in  : ".$countryName."</h3>";?></h2>
 			<?php include('cities_table.php');?>
 			</div>
         </div>
--- location/personDetails.php
+++ location/personDetails.php
@@ -29,14 +29,14 @@ $person = array_shift($result->items);
                     
                     <table border = '0' width='100%'>
                     <tr><td><b><font size='30px'><?php echo $person->firstname ." ". $person->lastname."";?></font></b></td><td><?php echo "<img src='".$person->profilpictureurl."'/>"?></td></tr>
-                    <tr><td><b>Nationality :</b> <?php echo $person->firstnationality->name." <img src='".$person->firstnationality->flagurl."'/>";?></td></tr>
+                    <tr><td><b>Nationality :</b> <?php echo $person->firstnationality->name." <A href='countryDetails.php?id=".$person->firstnationality->id."'><img src='".$person->firstnationality->flagurl."'/></A>";?></td></tr>
                     <tr><td><b>Hand :</b> <?php echo $person->hand;?></td></tr>
                     <tr><td><b>Height :</b> <?php echo $person->height;?></td></tr>
                      
                      <?php 
                      if(isset($person->birthplacecity->name)){
                          ?>
-                         <tr><td><b>Birthplace :</b> <?php echo "<A href='cityDetails.php?id=".$person->birthplacecity->id."'>".$person->birthplacecity->name."</A> (".$person->birthplacecity->country->name."<img src='".$person->birthplacecity->country->flagurl."'/>)";?></td></tr>
+                         <tr><td><b>Birthplace :</b> <?php echo "<A href='cityDetails.php?id=".$person->birthplacecity->id."'>".$person->birthplacecity->name."</A> (".$person->birthplacecity->country->name."<A href='countryDetails.php?id=".$person->firstnationality->id."'><img src='".$person->birthplacecity->country->flagurl."'/></A>)";?></td></tr>
                          <?php
                      }
                      ?>
